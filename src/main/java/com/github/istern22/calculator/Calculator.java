d41a3335631b6d5b3c8ef5a401cc20f86a4579ea
Istern22 <alistern22@gmail.com>
2019-10-17 17:51:47 +1000

    Scheme of Calculator

---
+++ src/main/java/com/github/istern22/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package com.github.istern22.calculator;
+
+import java.util.ArrayList;
+
+public class Calculator {
+
+    public Calculator(String input) {
+        //TODO
+    }
+
+    public ArrayList<Character> toReversePolishNotation(String input) {
+        return null;
+    }
+
+    public double calculate(ArrayList<Character> toCalculate) {
+        return 0;
+    }
+}
