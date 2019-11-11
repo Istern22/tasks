9c71836498a970ffad45fa61ec45149b19718557
Istern22 <alistern22@gmail.com>
2019-11-11 22:53:32 +1000

    Задачи. Мезенцев.

--- src/main/java/com/github/istern22/mezentsev/VectorArray.java
+++ src/main/java/com/github/istern22/mezentsev/VectorArray.java
@@ -1,5 +1,8 @@
 package com.github.istern22.mezentsev;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.github.istern22.mezentsev.Naturals.isPrimeNatural;
 
 public class VectorArray {
@@ -20,17 +23,102 @@ public class VectorArray {
         return result;
     }
 
-    public static double positiveAverage(double[] array) {
-        double average;
-        int count = 0;
-        double sum = 0;
+    /**
+     * Даны действительные числа с1, с2, ..., сn.
+     *  Найти произведение среднего арифметического положительных чисел
+     *  и среднее арифметического отрицательных чисел.
+     * @param array
+     * @return
+     */
+    public static double multiplyAverages(double[] array) {
+        int countPositive = 0;
+        int countNegative = 0;
+        double sumPositive = 0;
+        double sumNegative = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > 0) {
-                sum += array[i];
+            if (array[i] >= 0) {
+                sumPositive += array[i];
+                countPositive++;
+            } else {
+                sumNegative += array[i];
+                countNegative++;
             }
-            count++;
+        }
+        return (sumPositive / countPositive) * (sumNegative / countNegative);
+    }
+
+    /**
+     * Даны действительные числа с1, с2, ..., сn.
+     * Найти произведение суммы чисел с четными индексами и суммы чисел с нечетными индексами.
+     * @param array
+     * @return
+     */
+    public static double multiplyOfSums(double[] array) {
+        double sumOdd = 0;
+        double sumEven = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                sumEven += array[i];
+            } else {
+                sumOdd += array[i];
+            }
+        }
+        return sumEven * sumOdd;
+    }
+
+    /**
+     * Даны действительные числа a1, a2, ..., an.
+     * Выяснить, есть ли среди них повторяющиеся числа,
+     * и найти среднее арифметическое без учета повторов.
+     * @param array
+     * @return
+     */
+    public static double averageWithoutDuplicates(double[] array) {
+        double sum = 0;
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] != array[j]) {
+                    sum += array[i];
+                    count++;
+                }
+            }
         }
         return sum / count;
     }
 
+    /**
+     * Даны действительные числа a1, a2, ..., an.
+     * Пусть М - наибольшее, а м-наименьшее этих чисел.
+     * Получить в порядке возрастания все целые из интервала(м, М),
+     * которые не входят в последовательность a1, a2, ..., an.
+     * @param array
+     * @return
+     */
+    public static List<Integer> numbersFromInterval(int[] array) {
+        List<Integer> result = new ArrayList<>();
+        int maxIndex = 0;
+        int minIndex = 0;
+        for (int i = 1; i < array.length; i++) {
+           if (array[i] > array[maxIndex]) {
+               maxIndex = i;
+           } else if (array[i] < array[minIndex]) {
+               minIndex = i;
+           }
+        }
+
+        for (int number = array[minIndex] + 1; number < array[maxIndex]; number++) {
+            boolean duplicate = false;
+            for (int i = 0; i < array.length; i++) {
+                if (number == array[i]) {
+                    duplicate = true;
+                    break;
+                }
+            }
+            if (!duplicate) {
+                result.add(number);
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/com/github/istern22/mezentsev/VectorArrayTest.java
+++ src/test/java/com/github/istern22/mezentsev/VectorArrayTest.java
@@ -5,25 +5,64 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static com.github.istern22.mezentsev.VectorArray.*;
 import static  org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class VectorArrayTest {
 
-    VectorArray vectorArray = new VectorArray();
-
     @Test
     public void whenPrimeArrayIncrease() {
-        assertThat(vectorArray.increasingPrimeArray(new int[] {3, 5}), is(true));
+        assertThat(increasingPrimeArray(new int[] {3, 5}), is(true));
     }
 
     @Test
     public void whenPrimeArrayNotIncrease() {
-        assertThat(vectorArray.increasingPrimeArray(new int[] {3, 5, 7, 11, 2}), is(false));
+        assertThat(increasingPrimeArray(new int[] {3, 5, 7, 11, 2}), is(false));
     }
 
     @Test
     public void whenNotPrime() {
-        assertThat(vectorArray.increasingPrimeArray(new int[] {3, 5, 8, 11}), is(false));
+        assertThat(increasingPrimeArray(new int[] {3, 5, 8, 11}), is(false));
+    }
+
+    @Test
+    public void whenMultiplyAveragesOne() {
+        assertThat(multiplyAverages(new double[] {3, 5, -8, 10}), is(-48.0));
+    }
+
+    @Test
+    public void whenMultiplyAveragesTwo() {
+        assertThat(multiplyAverages(new double[] {0, -6, -8, 10}), is(-35.0));
+    }
+
+    @Test
+    public void whenMultiplySums() {
+        assertThat(multiplyOfSums(new double[] {0, -6, -8, 10}), is(-32.0));
+    }
+
+    @Test
+    public void whenMultiplySumsTwo() {
+        assertThat(multiplyOfSums(new double[] {0, -6, 0, 10}), is(0.0));
+    }
+
+    @Test
+    public void whenAverageWithoutDuplicates() {
+        assertThat(averageWithoutDuplicates(new double[] {1, 3, 5, 3, 6, 3, 0}), is(3.0));
+    }
+
+    @Test
+    public void whenAverageWithoutDuplicatesTwo() {
+        assertThat(averageWithoutDuplicates(new double[] {1, 3, 1, 3, 1, 3}), is(2.0));
+    }
+
+    @Test
+    public void whenFindIntegersBetweenMinAndMax() {
+        assertThat(numbersFromInterval(new int[] {1, 3}), is(new ArrayList<Integer>(Arrays.asList(2))));
+    }
+
+    @Test
+    public void whenFindIntegersBetweenMinAndMaxTwo() {
+        assertThat(numbersFromInterval(new int[] {1, 0, 3, 10, 8, 7}), is(new ArrayList<Integer>(Arrays.asList(2, 4, 5, 6, 9))));
     }
 }
