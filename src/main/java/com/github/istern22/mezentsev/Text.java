3af0fa71788d10d0551711095872df94197bfbb7
Istern22 <alistern22@gmail.com>
2019-12-05 18:17:43 +1000

    Задачи. Мезенцев.

--- src/main/java/com/github/istern22/mezentsev/Text.java
+++ src/main/java/com/github/istern22/mezentsev/Text.java
@@ -1,12 +1,11 @@
 package com.github.istern22.mezentsev;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Text {
 
-    public final static char[] vowels = {'А', 'а', 'О', 'о', 'И', 'и', 'Е', 'е', 'Э', 'э', 'Ы', 'ы', 'У', 'у', 'Ю', 'ю', 'Я', 'я'};
-    public final static char[] consonants = {'А', 'а', 'О', 'о', 'И', 'и', 'Е', 'е', 'Э', 'э', 'Ы', 'ы', 'У', 'у', 'Ю', 'ю', 'Я', 'я'};
+    public final static char[] vowels = {'а', 'о', 'и', 'е', 'э', 'ы', 'у', 'ю', 'я'};
+    public final static char[] consonants = {'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'};
 
     /**
      * В текстовом окне задан многострочный текст. Слова отделяются друг от друга пробелами.
@@ -17,9 +16,23 @@ public class Text {
      */
     public static List<String> function1(String text) {
         List<String> result = new ArrayList<>();
-        String[] words = text.split("[. ]");
+        String[] words = text.split("[ .]");
         for (int i = 0; i < words.length; i++) {
-
+            char[] word = words[i].toCharArray();
+            int countVowels = 0;
+            int countConsonants = 0;
+            for (int j = 0; j < word.length; j++) {
+                if (Arrays.asList(vowels).contains(word[i])) {
+                    countVowels++;
+                } else {
+                    if (Arrays.asList(consonants).contains(word[i])) {
+                        countConsonants++;
+                    }
+                }
+            }
+            if (countVowels >= countConsonants) {
+                result.add(words[i]);
+            }
         }
         return result;
     }
