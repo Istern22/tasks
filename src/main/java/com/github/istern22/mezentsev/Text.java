02329eb7c524707edcb0c72ec608df94c9b69de9
Istern22 <alistern22@gmail.com>
2019-12-06 18:17:22 +1000

    Задачи. Мезенцев.

--- src/main/java/com/github/istern22/mezentsev/Text.java
+++ src/main/java/com/github/istern22/mezentsev/Text.java
@@ -39,4 +39,187 @@ public class Text {
         }
         return result;
     }
+
+    /**
+     * Последовательно выделять слова, где согласных букв больше чем гласных.
+     * @param text
+     * @return
+     */
+    public static List<String> function2(String text) {
+        List<String> result = new ArrayList<>();
+        String[] words = text.replaceAll("[.]", "").split("[ ]");
+        for (int i = 0; i < words.length; i++) {
+            char[] word = words[i].toCharArray();
+            int countVowels = 0;
+            int countConsonants = 0;
+            for (int j = 0; j < word.length; j++) {
+                if (word[j] != ' ') {
+                    Character x = Character.toLowerCase(word[j]);
+                    if (vowelsHash.contains(x)) {
+                        countVowels++;
+                    } else if (consonantsHash.contains(x)) {
+                        countConsonants++;
+                    }
+                }
+            }
+            if (countVowels < countConsonants) {
+                result.add(words[i]);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Последовательно выделять предложения с четным количеством слов.
+     * @param text
+     * @return
+     */
+    public static List<String> function3(String text) {
+        List<String> result = new ArrayList<>();
+        String[] sentences = text.split("[.]");
+        for (int i = 0; i < sentences.length; i++) {
+            sentences[i] = sentences[i].trim();
+            String[] words = sentences[i].split("[ ]");
+            if (words.length % 2 == 0) {
+                result.add(sentences[i] + ".");
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Последовательно выделять предложения с нечетным количеством слов.
+     * @param text
+     * @return
+     */
+    public static List<String> function4(String text) {
+        List<String> result = new ArrayList<>();
+        String[] sentences = text.split("[.]");
+        for (int i = 0; i < sentences.length; i++) {
+            sentences[i] = sentences[i].trim();
+            String[] words = sentences[i].split("[ ]");
+            if (words.length % 2 == 1) {
+                result.add(sentences[i] + ".");
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Последовательно выделять слова с удвоенной гласной.
+     * @param text
+     * @return
+     */
+    public static List<String> function5(String text) {
+        List<String> result = new ArrayList<>();
+        String[] words = text.split("[ .]");
+        for (int i = 0; i < words.length; i++) {
+            char[] word = words[i].toCharArray();
+            for (int j = 0; j < word.length - 1; j++) {
+                if (word[j] != ' ' && vowelsHash.contains(word[j]) && word[j] == word[j + 1]) {
+                    result.add(words[i]);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Последовательно выделять самое короткое слово в каждом предложении.
+     * @param text
+     * @return
+     */
+    public static List<String> function6(String text) {
+        List<String> result = new ArrayList<>();
+        String[]sentences = text.split("[.]");
+        for (int i = 0; i < sentences.length; i++) {
+            sentences[i] = sentences[i].trim();
+            String[] words = sentences[i].split("[ ]");
+            String min = words[0];
+            for (int j = 1; j < words.length; j++) {
+                if (min.length() > words[j].length()) {
+                    min = words[j];
+                }
+            }
+            result.add(min);
+        }
+        return result;
+    }
+
+    /**
+     * Последовательно выделять самое длинное слово в каждом предложении.
+     * @param text
+     * @return
+     */
+    public static List<String> function7(String text) {
+        List<String> result = new ArrayList<>();
+        String[] sentences = text.split("[.]");
+        for (int i = 0; i < sentences.length; i++) {
+            sentences[i] = sentences[i].trim();
+            String[] words = sentences[i].split("[ ]");
+            String max = words[0];
+            for (int j = 1; j < words.length; j++) {
+                if (max.length() < words[j].length()) {
+                    max = words[j];
+                }
+            }
+            result.add(max);
+        }
+        return result;
+    }
+
+    /**
+     * Последовательно выделять слова с удвоенной согласной.
+     * @param text
+     * @return
+     */
+    public static List<String> function8(String text) {
+        List<String> result = new ArrayList<>();
+        String[] words = text.split("[ .]");
+        for (int i = 0; i < words.length; i++) {
+            char[] word = words[i].toCharArray();
+            for (int j = 0; j < word.length - 1; j++) {
+                if (word[j] != ' ' && consonantsHash.contains(word[j]) && word[j] == word[j + 1]) {
+                    result.add(words[i]);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Последовательно выделять слова длина которых больше заданного числа.
+     * @param text
+     * @param number
+     * @return
+     */
+    public static List<String> function9(String text, int number) {
+        List<String> result = new ArrayList<>();
+        String[] words = text.split("[ .]");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() > number) {
+                result.add(words[i]);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Последовательно выделять слова длина которых меньше заданного числа.
+     * @param text
+     * @param number
+     * @return
+     */
+    public static List<String> function10(String text, int number) {
+        List<String> result = new ArrayList<>();
+        String[] words = text.split("[ .]");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() != 0 && words[i].length() < number ) {
+                result.add(words[i]);
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/com/github/istern22/mezentsev/TextTest.java
+++ src/test/java/com/github/istern22/mezentsev/TextTest.java
@@ -13,10 +13,66 @@ import static org.junit.Assert.assertThat;
 public class TextTest {
     public final String text =
             "Сжалился волк. Пустил лису. Первую ночь лиса на приступочке спала. На вторую забралась в избу. А на третью на печи развалилась.";
+    public final String text1 =
+            "Сжалился длинный волк в зоопарке. Пустил деревянную лису. Первую ночь лиса на приступочке спала. На вторую забралась в избу. А на третью в воображении на топленной печи развалилась.";
 
     @Test
     public void whenFunction1() {
         List<String> expected = new ArrayList<>(Arrays.asList("лису", "Первую", "лиса", "на", "На", "вторую", "избу", "А", "на", "на", "печи"));
         assertThat(function1(text), is(expected));
+    }
+
+    @Test
+    public void whenFunction2() {
+        List<String> expected = new ArrayList<>(Arrays.asList("Сжалился", "волк", "Пустил", "ночь", "приступочке", "спала", "забралась", "в", "третью", "развалилась"));
+        assertThat(function2(text), is(expected));
+    }
+
+    @Test
+    public void whenFunction3() {
+        List<String> expected = new ArrayList<>(Arrays.asList("Сжалился волк.", "Пустил лису.", "Первую ночь лиса на приступочке спала.", "А на третью на печи развалилась."));
+        assertThat(function3(text), is(expected));
+    }
+
+    @Test
+    public void whenFunction4() {
+        List<String> expected = new ArrayList<>(Arrays.asList("На вторую забралась в избу."));
+        assertThat(function4(text), is(expected));
+    }
+
+    @Test
+    public void whenFunction5() {
+        List<String> expected = new ArrayList<>(Arrays.asList("зоопарке", "воображении"));
+        assertThat(function5(text1), is(expected));
+    }
+
+    @Test
+    public void whenFunction6() {
+        List<String> expected = new ArrayList<>(Arrays.asList("волк", "лису", "на", "в", "А"));
+        assertThat(function6(text), is(expected));
+    }
+
+    @Test
+    public void whenFunction7() {
+        List<String> expected = new ArrayList<>(Arrays.asList("Сжалился", "Пустил", "приступочке", "забралась", "развалилась"));
+        assertThat(function7(text), is(expected));
+    }
+
+    @Test
+    public void whenFunction8() {
+        List<String> expected = new ArrayList<>(Arrays.asList("длинный", "деревянную", "топленной"));
+        assertThat(function8(text1), is(expected));
+    }
+
+    @Test
+    public void whenFunction9() {
+        List<String> expected = new ArrayList<>(Arrays.asList("Сжалился", "приступочке", "забралась", "развалилась"));
+        assertThat(function9(text, 6), is(expected));
+    }
+
+    @Test
+    public void whenFunction10() {
+        List<String> expected = new ArrayList<>(Arrays.asList("на", "На", "в", "А", "на", "на"));
+        assertThat(function10(text, 3), is(expected));
     }
 }
