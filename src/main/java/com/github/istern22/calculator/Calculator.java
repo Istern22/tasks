3b09aeacdd0cc99d99a7fc4e6f80c08babf27085
Istern22 <alistern22@gmail.com>
2020-02-02 22:57:34 +1000

    Calculator

--- src/main/java/com/github/istern22/calculator/Calculator.java
+++ src/main/java/com/github/istern22/calculator/Calculator.java
@@ -1,12 +1,28 @@
 package com.github.istern22.calculator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Calculator {
 
-    public boolean checkString(String element) {
+    private static final String LEFT_BRACKET = "(";
+    private static final String RIGHT_BRACKET = ")";
+    private static final String PLUS = "+";
+    private static final String MINUS = "-";
+    private static final String MULT = "*";
+    private static final String DIV = "/";
+    private static final String EXP = "^";
+
+    private static final Map<String, Integer> PRIORITY = Map.of(
+            LEFT_BRACKET, 0,
+            RIGHT_BRACKET, 1,
+            PLUS, 2,
+            MINUS, 2,
+            MULT, 3,
+            DIV, 3,
+            EXP, 4
+    );
+
+    public static boolean isDouble(String element) {
         try {
             Double.parseDouble(element);
         } catch (Exception e) {
@@ -15,19 +31,76 @@ public class Calculator {
         return true;
     }
 
-    public List<Double> getOperands(String input) {
-
-        List<Double> operands = new ArrayList<>();
-
-        List<String> sourceArray = new ArrayList<String>(Arrays.asList(input.split("")));
-
-        return operands;
+    public static String convertToRpn(String expression) {
+        var items = expression.split(" ");
+        var operators = new ArrayList<String>();
+        var result = "";
+        for (var item : items) {
+           if (isDouble(item)) {
+               result += item + " ";
+           } else if (PRIORITY.keySet().contains(item)) {
+               if (operators.isEmpty()) {
+                   operators.add(item);
+               } else if (item.equals(LEFT_BRACKET)) {
+                   operators.add(item);
+               } else if (item.equals(RIGHT_BRACKET)) {
+                    for (int i = operators.size() - 1; i >= 0; i--) {
+                        if (!operators.get(i).equals(LEFT_BRACKET)) {
+                            result += operators.get(i) + " ";
+                            operators.remove(i);
+                        }
+                    }
+               } else if (!operators.isEmpty()) {
+                   for (int i = operators.size() - 1; i >= 0; i--) {
+                       if (PRIORITY.get(operators.get(i)) >= PRIORITY.get(item)) {
+                           result += operators.get(i) + " ";
+                           operators.remove(i);
+                       } else {
+                           operators.add(item);
+                           break;
+                       }
+                   }
+               }
+           } else {
+               throw new ArithmeticException("Боля-боля.");
+           }
+        }
+        return result.trim();
     }
 
-    public double calculate(String input) {
-
-        Double result = null;
-
-        return result;
+    public static double calculate(String expression) {
+        var result = new ArrayList<Double>();
+        var items = convertToRpn(expression).split(" ");
+        for (var item : items) {
+            if (isDouble(item)) {
+                result.add(Double.parseDouble(item));
+            } else {
+                switch (item) {
+                    case PLUS :
+                        result.set(result.size() - 2, result.get(result.size() - 2) + result.get(result.size() - 1));
+                        result.remove(result.size() - 1);
+                    break;
+                    case MINUS :
+                        result.set(result.size() - 2, result.get(result.size() - 2) - result.get(result.size() - 1));
+                        result.remove(result.size() - 1);
+                        break;
+                    case MULT :
+                        result.set(result.size() - 2, result.get(result.size() - 2) * result.get(result.size() - 1));
+                        result.remove(result.size() - 1);
+                        break;
+                    case DIV :
+                        result.set(result.size() - 2, result.get(result.size() - 2) / result.get(result.size() - 1));
+                        result.remove(result.size() - 1);
+                        break;
+                    /**case "^" :
+                        result.set(0, Math.pow(result.get(0), );
+                        result.remove(1);
+                        break;*/
+                    default:
+                        break;
+                }
+            }
+        }
+        return result.get(0);
     }
 }
--- src/test/java/com/github/istern22/calculator/CalculatorTest.java
+++ src/test/java/com/github/istern22/calculator/CalculatorTest.java
@@ -8,6 +8,17 @@ import static org.junit.Assert.assertThat;
 public class CalculatorTest {
 
     @Test
-    public void whenGetNumbersOne() {
+    public void whenConvertToRpn() {
+        assertThat(Calculator.convertToRpn("( 8 + 2 * 5 ) / ( 1 + 3 * 2 - 4 )"), is("8 2 5 * + 1 3 2 * + 4 - /"));
+    }
+
+   /** @Test
+    public void whenConvertToRpnExp() {
+        assertThat(Calculator.convertToRpn("2 ^ 3"), is("2 3 ^"));
+    }*/
+
+    @Test
+    public void whenCalculate() {
+        assertThat(Calculator.calculate("( 8 + 2 * 5 ) / ( 1 + 3 * 2 - 4 )"), is(6.0));
     }
 }
