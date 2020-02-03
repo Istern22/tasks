610cc25552cc12dd98ce508bdb4e04c593d7f955
Istern22 <alistern22@gmail.com>
2020-02-03 12:44:02 +1000

    Калькулятор (Польская обратная запись)

--- src/main/java/com/github/istern22/calculator/Calculator.java
+++ src/main/java/com/github/istern22/calculator/Calculator.java
@@ -32,38 +32,44 @@ public class Calculator {
     }
 
     public static String convertToRpn(String expression) {
-        var items = expression.split(" ");
+        String[] items = expression.split(" ");
         var operators = new ArrayList<String>();
         var result = "";
-        for (var item : items) {
-           if (isDouble(item)) {
-               result += item + " ";
-           } else if (PRIORITY.keySet().contains(item)) {
-               if (operators.isEmpty()) {
-                   operators.add(item);
-               } else if (item.equals(LEFT_BRACKET)) {
-                   operators.add(item);
-               } else if (item.equals(RIGHT_BRACKET)) {
-                    for (int i = operators.size() - 1; i >= 0; i--) {
-                        if (!operators.get(i).equals(LEFT_BRACKET)) {
-                            result += operators.get(i) + " ";
-                            operators.remove(i);
+        for (int i = 0; i < items.length; i++) {
+            if (isDouble(items[i])) {
+                result += items[i] + " ";
+            } else if (PRIORITY.keySet().contains(items[i])) {
+                if (operators.isEmpty() || items[i].equals(LEFT_BRACKET)) {
+                    operators.add(items[i]);
+                } else if (items[i].equals(RIGHT_BRACKET)) {
+                    for (int j = operators.size() - 1; j >= 0; j--) {
+                        if (!operators.get(j).equals(LEFT_BRACKET)) {
+                            result += operators.get(j) + " ";
+                            operators.remove(j);
+                        } else {
+                            operators.remove(LEFT_BRACKET);
                         }
                     }
-               } else if (!operators.isEmpty()) {
-                   for (int i = operators.size() - 1; i >= 0; i--) {
-                       if (PRIORITY.get(operators.get(i)) >= PRIORITY.get(item)) {
-                           result += operators.get(i) + " ";
-                           operators.remove(i);
-                       } else {
-                           operators.add(item);
-                           break;
-                       }
-                   }
-               }
-           } else {
-               throw new ArithmeticException("Боля-боля.");
-           }
+                } else if (!operators.isEmpty()) {
+                    for (int j = operators.size() - 1; j >= 0; j--) {
+                        if (PRIORITY.get(operators.get(j)) >= PRIORITY.get(items[i])) {
+                            result += operators.get(j) + " ";
+                            operators.remove(j);
+                        } else {
+                            operators.add(items[i]);
+                            break;
+                        }
+                    }
+                } else {
+                    throw new ArithmeticException("Ошибка в выражении.");
+                }
+            }
+            if (i == items.length - 1) {
+                for (int j = operators.size() - 1; j >= 0; j--) {
+                    result += operators.get(j) + " ";
+                    operators.remove(j);
+                }
+            }
         }
         return result.trim();
     }
@@ -92,10 +98,10 @@ public class Calculator {
                         result.set(result.size() - 2, result.get(result.size() - 2) / result.get(result.size() - 1));
                         result.remove(result.size() - 1);
                         break;
-                    /**case "^" :
-                        result.set(0, Math.pow(result.get(0), );
-                        result.remove(1);
-                        break;*/
+                    case EXP :
+                        result.set(result.size() - 2, Math.pow(result.get(result.size() - 2), result.get(result.size() - 1)));
+                        result.remove(result.size() - 1);
+                        break;
                     default:
                         break;
                 }
--- src/test/java/com/github/istern22/calculator/CalculatorTest.java
+++ src/test/java/com/github/istern22/calculator/CalculatorTest.java
@@ -12,13 +12,33 @@ public class CalculatorTest {
         assertThat(Calculator.convertToRpn("( 8 + 2 * 5 ) / ( 1 + 3 * 2 - 4 )"), is("8 2 5 * + 1 3 2 * + 4 - /"));
     }
 
-   /** @Test
-    public void whenConvertToRpnExp() {
+   @Test
+    public void whenConvertToRpn1() {
         assertThat(Calculator.convertToRpn("2 ^ 3"), is("2 3 ^"));
-    }*/
+    }
 
     @Test
-    public void whenCalculate() {
+    public void whenConvertToRpn2() {
+        assertThat(Calculator.convertToRpn("2 ^ ( 3 + 1 )"), is("2 3 1 + ^"));
+    }
+
+    @Test
+    public void whenCalculate0() {
         assertThat(Calculator.calculate("( 8 + 2 * 5 ) / ( 1 + 3 * 2 - 4 )"), is(6.0));
+    }
+
+    @Test
+    public void whenCalculate1() {
+        assertThat(Calculator.calculate("2 ^ 3"), is(8.0));
+    }
+
+    @Test
+    public void whenCalculate2() {
+        assertThat(Calculator.calculate("2 ^ ( 3 + 1 )"), is(16.0));
+    }
+
+    @Test
+    public void whenCalculate3() {
+        assertThat(Calculator.calculate("2 * ( 3 + 1 )"), is(8.0));
     }
 }
